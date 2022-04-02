5f3b0417557482955a9ddb3fd608dc1aeca8f234
fasbrothers <nigmatovsanjar00@gmail.com>
2022-04-02 10:44:31 +0500

    models

---
+++ DBSD_CW/Models/Staff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DBSD_CW.Models
+{
+    public class Staff
+    {
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Salary { get; set; }
+        public int BranchId { get; set; }
+    }
+}
