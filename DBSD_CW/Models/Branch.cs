48b5829631d84b8a3f5973bd1bba81b2f738b4ea
Ulugbek Turakulov <ulugbek.turaqulov311@gmail.com>
2022-04-02 10:39:37 +0500

    created models

---
+++ DBSD_CW/Models/Branch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DBSD_CW.Models
+{
+    public class Branch
+    {
+        public int BranchId { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
